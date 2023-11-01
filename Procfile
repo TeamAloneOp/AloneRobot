9a68f6b28e850ed65b25807f8000b51c5e0d237a
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2023-11-01 08:01:48 +0530

    Mr Alone

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m AloneRobot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE Alone Robot
+:: Enables virtual env mode and then starts Alone
+env\scripts\activate.bat && py -m AloneRobot
