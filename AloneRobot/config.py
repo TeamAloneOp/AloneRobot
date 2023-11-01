8ec1bd1f841b9dba958ef1ed52852cc70b0c7555
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2023-11-01 08:42:39 +0530

    Update config.py

--- AloneRobot/config.py
+++ AloneRobot/config.py
@@ -10,7 +10,7 @@ class Config(object):
     OWNER_ID = 6079943111 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
     SUPPORT_CHAT = "AlonesHeaven"  # Your own group for support, do not add the @
-    START_IMG = "https://telegra.ph/file/1154edfd42aa2410e017b.jpg"
+    START_IMG = ""
     EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     MONGO_DB_URI= ""
     # RECOMMENDED
